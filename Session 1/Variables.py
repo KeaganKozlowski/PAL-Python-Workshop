3c693c12961b932f44f95f55d73f996ac270185d
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 20:45:32 +0100

    Create Variables.py

---
+++ Session 1/Variables.py
@@ -0,0 +1,11 @@
+#Syntax
+variable_name = value
+#Example 1
+x = 10 #Type doesn't have to be defined as python presumes it is an integer
+name = "John" #Type doesn't have to be defined as python presumes it is an String
+#Example 2 of different naming conventions
+first_name = "John" #Snake case preferred in Python
+firstName = "John" #Camel case
+FirstName = "John" #Pascal case (Upper Camel case)
+first-name = "John" #Kebab case (Cannot use in Python hyphens not allowed in variables)
+FIRST_NAME = "John" #Uppercase
