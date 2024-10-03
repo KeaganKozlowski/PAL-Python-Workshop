b0df4bd806371f37732d6defb427d626a76f2e9e
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 21:05:41 +0100

    Create Functions.py

---
+++ Session 1/Functions.py
@@ -0,0 +1,9 @@
+#Syntax
+def function_name(parameters):
+  #Code to run
+  return value #function doesnt always have to return a value
+#Example
+def double(value):
+  result = value * 2
+  return result
+print(double(2)) #Will output 4
