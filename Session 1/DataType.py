a351993d533039ce0c030fa044900937696ad829
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 19:35:13 +0100

    Create DataType.py

---
+++ Session 1/DataType.py
@@ -0,0 +1,9 @@
+#Showing how different types of numbers can have different types in Python
+##This should have a type of int - Integer
+print(type(10))
+##This should have a type of float - Float/Decimal number
+print(type(1.1))
+##Even though the number is negative doesn't change it's type, will have type int - Integer
+print(type(-42))
+
+#
