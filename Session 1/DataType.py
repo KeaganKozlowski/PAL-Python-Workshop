852920bc6657f88631ec95251da9bd8b8ff9fa0b
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 20:40:34 +0100

    Update DataType.py

--- Session 1/DataType.py
+++ Session 1/DataType.py
@@ -5,5 +5,15 @@ print(type(10))
 print(type(1.1))
 ##Even though the number is negative doesn't change it's type, will have type int - Integer
 print(type(-42))
-
-#
+#This should have a type of str - String
+print(type("Hello World"))
+#This should have a type of bool - Boolean
+print(type(True))
+#This should have a type of list
+print(type([1,2,3,4]))
+#This should have a type of dict - Dictionary
+print(type({"Age":18,"Name":"John"}))
+#This should have a type of tuple
+print(type((1,2,3)))
+#This should have a type of set
+print(type({1,2,3}))
