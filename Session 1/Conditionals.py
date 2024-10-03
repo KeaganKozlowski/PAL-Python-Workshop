08cfe5356e67f6ed7d5cc619485d0467a016f86f
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 20:52:54 +0100

    Create Conditionals.py

---
+++ Session 1/Conditionals.py
@@ -0,0 +1,13 @@
+#Syntax
+if condition:
+  #Code to run if condition is True
+elif another_condition:
+  #Code to run if another_condition is True
+else:
+  #Code to run if all conditions are False
+#Example
+age = 19
+if age >= 18:
+  print("You can vote")
+else:
+  print("You are too young to vote")
