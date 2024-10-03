51f5b3ad34970cdba47d2e6abe0393059031ba20
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-03 20:56:58 +0100

    Create Loops.py

---
+++ Session 1/Loops.py
@@ -0,0 +1,17 @@
+#For Loops
+##For i in range
+for i in range(n): #Where n can be any number or variable
+  print(i) #Should output from 0 to n - 1
+  #Code to be ran
+##For each
+for item in sequence: #Item is just a variable name it can be named anything
+  print(item) #Will output each item in the sequence
+  #Code to be ran
+#While loops
+while condition:
+  #Code to be ran
+##Example
+count = 0
+while count < 5:
+  print(count) #Will output from 0 to 4
+  count += 1
