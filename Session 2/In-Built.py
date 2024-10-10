49c4a5b9569ba7fd7728ffdea6a80cd01e8844c1
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-10 18:54:47 +0100

    Create In-Built.py

---
+++ Session 2/In-Built.py
@@ -0,0 +1,16 @@
+#Math
+import math
+##Can be used to improve readability of code
+##But also more complex mathematical functions
+###For example floor division
+x = 5
+y = 2
+print(x//y) #Outputs 2
+print(math.floor(x,y)) #Outputs 2
+
+
+#Random
+##Allows for generation of random numbers
+import random
+#. is referencing a specific function
+print(random.randint(1,10)) #Output random number between 1 and 10
