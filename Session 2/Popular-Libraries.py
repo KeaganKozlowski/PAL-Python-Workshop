cdbc932544f70b944011e29cbcd0a88a40565763
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-10-10 19:03:14 +0100

    Create Popular-Libraries.py

---
+++ Session 2/Popular-Libraries.py
@@ -0,0 +1,10 @@
+#NumPy - Numerical Computing
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr * 2)  #Multiplies each element by 2
+
+#Pandas - Data manipulation and Analysis
+import pandas as pd
+data = {'Name': ['Alice', 'Bob'], 'Age': [25, 30]}
+df = pd.DataFrame(data)
+print(df)
