98b6f7dd5861c0a84982c1d2ea253c581b06de7c
Keagan Kozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-11-08 11:22:28 +0000

    Create CreateMatrices.py

---
+++ Session 5/CreateMatrices.py
@@ -0,0 +1,16 @@
+#Using 2 dimensional lists
+matrix1 = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+print(matrix1)
+
+#Using Numerical Python
+import numpy as np
+matrix2 = np.array([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+])
+print(matrix2)
